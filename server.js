9ec7a6c291e0c3a6dd1df958608145627b500042
cosmolevano-source <cosmolevano@gmail.com>
2026-03-13 11:27:30 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,27 @@
+const express = require("express");
+const app = express();
 
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Server spedizioni attivo");
+});
+
+app.post("/spedizione", (req, res) => {
+
+  const dati = req.body;
+
+  console.log("Dati spedizione:", dati);
+
+  res.json({
+    status: "ok",
+    messaggio: "spedizione ricevuta"
+  });
+
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Server attivo sulla porta " + PORT);
+});
